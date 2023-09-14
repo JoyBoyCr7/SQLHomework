fef4b3d722ba8fd74517336f52d3cffeeaf30ef9
JoyBoyCr7 <Rnyongkah@gmail.com>
2023-09-13 22:24:09 -0500

    I found Carmen Sandiego

--- find_carman.sql
+++ find_carman.sql
@@ -72,7 +72,8 @@ WHERE id = 211
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
-
-
+SElECT name
+FROM city
+WHERE population = 91084
 
 -- She's in ____________________________!
